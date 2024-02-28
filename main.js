e55c4624e9be0f5169970d06b91007208b15cc7c
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-02-28 05:00:58 -0300

    feat: menu fixed scroll size

---
+++ main.js
@@ -0,0 +1,4 @@
+
+const nav = document.getElementById("navbar");  
+
+window.addEventListener( "scroll", () => (window.scrollY >= 250) ? nav.classList.add('active') : nav.classList.remove('active'))
